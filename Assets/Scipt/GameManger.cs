1246b553124dcfdb39859264baa4f2dde3008038
emily <emily91031532@gmail.com>
2023-07-05 20:31:09 +0800

    Initial commit

---
+++ Assets/Scipt/GameManger.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManger : MonoBehaviour
+{
+    public static GameManger m_Main = null;
+    public UnityEngine.UI.Text m_LiveValue = null;
+    public UnityEngine.UI.Text m_Score = null;
+    public MangerUnit[] m_AllUnit=null;
+    public int m_NowScore = 0;
+    private int m_Live = 5;
+    private bool m_PlayerLive = true;
+
+    private int m_HankRank = 1;
+    private void Awake()
+    {
+        m_Main = this;
+    }
+    public int GetHardRank()
+    {
+        return m_HankRank;
+    }
+    public void Hit()
+    {
+        m_Live -= 1;
+        if (m_Live < 0)
+        {
+            m_Live = 0;
+        }
+        m_LiveValue.text = m_Live.ToString();
+        if (m_Live <= 0)
+        {
+            PlayerDie();
+        }
+    }
+
+    private void PlayerDie()
+    {
+        m_PlayerLive = false;
+    }
+    public void AddScore(int score)
+    {
+        m_NowScore += score;
+        if (m_HankRank == 1 && m_NowScore > 5)
+        {
+            m_HankRank = 1;//打一下
+        }
+        else if(m_HankRank == 2 && m_NowScore > 30)
+        {
+            m_HankRank = 2;//打兩下
+        }
+        m_Score.text = m_NowScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_PlayerLive == false)
+        {
+            return;
+        }
+        for(int i = 0; i < m_AllUnit.Length; i++)
+        {
+            MangerUnit unit = m_AllUnit[i];
+            if (unit.CheckTime() == true)
+            {
+                unit.Reburn();
+
+            }
+            else 
+            {
+
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scipt/MangerUnit.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MangerUnit : MonoBehaviour
+{
+
+    private float m_Time = 0;
+    private bool m_IsLive = false;
+    private bool m_IsCanClick = false;
+
+    private int m_Hp = 0;
+    private float m_RemoveTime = 0;
+    private UnityEngine.UI.Text m_HitTimes = null;
+    private void Awake()
+    {
+        m_HitTimes = gameObject.GetComponentInChildren<UnityEngine.UI.Text>();
+        FindObjectsOfType<MangerUnit>();
+    }
+    public void OnClickMonster()
+    {
+        if (m_IsCanClick == false)
+        {
+            return;
+        }
+        m_Hp --;
+        m_HitTimes.text = m_Hp.ToString();
+        if (m_Hp <= 0)
+        {
+            GameManger.m_Main.AddScore(1);
+            OnDie();
+        }
+
+    }
+    private void OnDie()
+    {
+        
+        gameObject.SetActive(false);
+        m_IsLive = false;
+        AddTime();
+    }
+    void Start()
+    {
+        AddTime();
+        //gameObject.SetActive(true);
+    }
+
+
+    void Update()
+    {
+      if(m_IsLive)
+        {
+            if (transform.localScale.x < 1)
+            {
+                transform.localScale += new Vector3(1f,1f, 1f) * Time.deltaTime;
+            }
+            else
+            {
+                m_IsCanClick = true;
+                
+            }
+            if (m_IsCanClick)
+            {
+                m_RemoveTime += Time.deltaTime;
+                int limitTime = 5 - GameManger.m_Main.GetHardRank();
+                if (m_RemoveTime > limitTime)
+                {
+                    GameManger.m_Main.Hit();
+                    OnDie();
+                }
+            }
+        }
+    }
+    public bool CheckTime()
+    {
+    if (Time.time > m_Time)
+            {
+                return true;
+            }
+            return false;
+    }
+    public void Reburn()
+    {
+        
+        if (m_IsLive ==true)
+        {
+            return;
+        }
+        AddTime();
+        m_RemoveTime = 0;
+        m_Hp = GameManger.m_Main.GetHardRank();
+        m_HitTimes.text = m_Hp.ToString();
+        m_RemoveTime = 0;
+        transform.localScale = new Vector3(1f, 1f, 1f);
+        m_IsLive = true;
+        m_IsCanClick = false;
+        gameObject.SetActive(true);
+
+    }
+     private void AddTime()
+    {
+        m_Time = Time.time + Random.Range(0.3f, 5f);
+    }   
+    
+}
