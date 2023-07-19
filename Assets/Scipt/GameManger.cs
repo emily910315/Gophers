744b6d17856f4633beb69e061576767af3e8a29a
emily <emily91031532@gmail.com>
2023-07-19 20:38:43 +0800

    修改遊戲
    1.可以成功倒數生命值
    2.點擊後沒有任何加分反應

--- Assets/Scipt/GameManger.cs
+++ Assets/Scipt/GameManger.cs
@@ -12,8 +12,47 @@ public class GameManger : MonoBehaviour
     public int m_NowScore = 0;
     private int m_Live = 5;
     private bool m_PlayerLive = true;
-
     private int m_HankRank = 1;
+
+    private bool isCountingDown = false; // 是否正在倒數
+    private float countdownTime = 5f; // 倒數的初始時間
+    private float countdownTimer = 0f; // 倒數計時器
+    private float lastClickTime = 0f; // 上次點擊的時間
+    private float doubleClickThreshold;//雙擊值域
+    private bool isDoubleClick = false; // 是否是雙擊
+    private void Start()
+    {
+        // 倒計時
+        StartCountdown();
+    }
+
+    private void StartCountdown()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    private IEnumerator Countdown()
+    {
+        m_Live = 5; // 生命初始值
+        m_LiveValue.text = m_Live.ToString();
+        yield return new WaitForSeconds(1f); // 等待1秒
+        // 從5開始倒數到0
+        for (int i = 5; i >= 0; i--)
+        { 
+
+            m_Live = i;
+            m_LiveValue.text = m_Live.ToString();
+            yield return new WaitForSeconds(1f); // 等待1秒
+        }
+        // 最後倒數到0
+        m_Live = 0;
+        m_LiveValue.text = m_Live.ToString();
+
+        if (m_Live <= 0)
+        {
+            PlayerDie();
+        }
+    }
     private void Awake()
     {
         m_Main = this;
@@ -24,11 +63,11 @@ public class GameManger : MonoBehaviour
     }
     public void Hit()
     {
-        m_Live -= 1;
+        m_Live -= 1;//生命值扣1
         if (m_Live < 0)
         {
             m_Live = 0;
-        }
+        }//生命值不會變負
         m_LiveValue.text = m_Live.ToString();
         if (m_Live <= 0)
         {
@@ -40,19 +79,47 @@ public class GameManger : MonoBehaviour
     {
         m_PlayerLive = false;
     }
+
+
+    public void OnClick()
+    {
+        float currentTime = Time.time;
+        // 當有點擊時，重置倒數計時器
+        countdownTimer = countdownTime;
+        isCountingDown = false;
+
+        if (currentTime - lastClickTime <= doubleClickThreshold)
+        {
+            // 雙擊
+            AddScore(2);
+            isDoubleClick = true;
+        }
+        else
+        {
+            // 單擊
+            isDoubleClick = false;
+        }
+
+        lastClickTime = currentTime;
+    }
+
+    
+
     public void AddScore(int score)
     {
-        m_NowScore += score;
-        if (m_HankRank == 1 && m_NowScore > 5)
+        if (m_PlayerLive==true)
         {
-            m_HankRank = 1;//打一下
+            // 如果是雙擊，增加兩倍分數
+            if (isDoubleClick)
+            {
+                score += 2;
+            }
+
+            m_NowScore += score;
+            m_Score.text = m_NowScore.ToString();
         }
-        else if(m_HankRank == 2 && m_NowScore > 30)
-        {
-            m_HankRank = 2;//打兩下
-        }
-        m_Score.text = m_NowScore.ToString();
     }
+
 
     // Update is called once per frame
     void Update()
@@ -61,19 +128,55 @@ public class GameManger : MonoBehaviour
         {
             return;
         }
-        for(int i = 0; i < m_AllUnit.Length; i++)
+
+        if (!isCountingDown)
         {
-            MangerUnit unit = m_AllUnit[i];
-            if (unit.CheckTime() == true)
-            {
-                unit.Reburn();
+            // 如果沒有在倒數，檢查是否需要開始倒數
+            bool hasClick = Input.GetMouseButtonDown(0);
 
-            }
-            else 
+            if (!hasClick)
             {
+                // 如果沒有點擊，開始倒數計時
+                countdownTimer -= Time.deltaTime;
 
+                if (countdownTimer <= 0f)
+                {
+                    // 開始倒數時，生命值減少
+                    m_Live--;
+
+                    if (m_Live < 0)
+                    {
+                        m_Live = 0;
+                    }
+
+                    m_LiveValue.text = m_Live.ToString();
+
+                    if (m_Live <= 0)
+                    {
+                        PlayerDie();
+                    }
+
+                    // 重置倒數計時器
+                    countdownTimer = countdownTime;
+                }
             }
-            
+            else
+            {
+                // 如果有點擊，增加分數並重置倒數計時器
+                AddScore(1); // 單擊增加一分
+                countdownTimer = countdownTime;
+            }
+
+            // 設定為正在倒數狀態
+            isCountingDown = true;
+        }
+        else
+        {
+            // 如果正在倒數，檢查是否有點擊，若有則重置倒數計時器
+            if (Input.GetMouseButtonDown(0))
+            {
+                countdownTimer = countdownTime;
+            }
         }
     }
 }
--- Assets/Scipt/MangerUnit.cs
+++ Assets/Scipt/MangerUnit.cs
@@ -10,8 +10,11 @@ public class MangerUnit : MonoBehaviour
     private bool m_IsLive = false;
     private bool m_IsCanClick = false;
 
-    private int m_Hp = 0;
+    private int m_Hp;//生命值
     private float m_RemoveTime = 0;
+    private int m_MaxHp;
+    private Dictionary<MangerUnit, int> m_MaxHpDict = new Dictionary<MangerUnit, int>();
+
 
     public void OnClickMonster()
     {
@@ -22,21 +25,43 @@ public class MangerUnit : MonoBehaviour
         m_Hp --;
         if (m_Hp <= 0)
         {
-            GameManger.m_Main.AddScore(1);
+            int score = (GameManger.m_Main.GetHardRank() == 1) ? 1 : 2;
+            GameManger.m_Main.AddScore(score);
             OnDie();
         }
 
     }
     private void OnDie()
     {
-        
+        StartCoroutine(FadeOut());
         gameObject.SetActive(false);
         m_IsLive = false;
         AddTime();
     }
+
+    private IEnumerator FadeOut()
+    {
+        float duration = 1.0f;
+        float elapsedTime = 0.0f;
+
+        while (elapsedTime < duration)
+        {
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration); // 計算透明度的插值
+            Color newColor = new Color(1f, 1f, 1f, alpha); // 根據透明度創建新的顏色
+            gameObject.GetComponent<Renderer>().material.color = newColor; // 將物體的顏色設定為新的顏色
+
+            elapsedTime += Time.deltaTime; // 更新已過去的時間
+            yield return null; // 等待下一幀
+        }
+
+        gameObject.SetActive(false); // 停用物體（隱藏物體）
+        m_IsLive = false; // 設定為非存活狀態
+        AddTime(); // 呼叫 AddTime 方法
+    }
     void Start()
     {
         AddTime();
+        m_Hp = m_MaxHp; // 設定地鼠的當前生命值為最大生命值
         //gameObject.SetActive(true);
     }
 
@@ -82,9 +107,16 @@ public class MangerUnit : MonoBehaviour
             return;
         }
         AddTime();
+
+        // 創建一個新的地鼠物件
+        GameObject newUnitObj = Instantiate(gameObject);
+        MangerUnit newUnit = newUnitObj.GetComponent<MangerUnit>();
+
+        // 設定新地鼠物件的最大生命值
+        newUnit.m_MaxHp = m_MaxHp;
+
         m_RemoveTime = 0;
-        m_Hp = GameManger.m_Main.GetHardRank();
-        m_RemoveTime = 0;
+        //m_Hp = GameManger.m_Main.GetHardRank();
         transform.localScale = new Vector3(1f, 1f, 1f);
         m_IsLive = true;
         m_IsCanClick = false;
