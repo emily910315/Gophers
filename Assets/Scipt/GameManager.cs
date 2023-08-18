93be469064585d17aa57f99edc158d143dcc21b8
emily <emily91031532@gmail.com>
2023-08-19 00:11:07 +0800

    修復 隨機產生按鈕
    - 按鈕可生成或消失
    - 給予按鈕指令會給予反映

--- Assets/Scipt/GameManager.cs
+++ Assets/Scipt/GameManager.cs
@@ -19,66 +19,66 @@ public class GameManager : MonoBehaviour
     }
 
 
-    public Text m_HpText = null;//生命值文字
-    public Text m_ScoreText = null;//分數文字
-    public ManagerUnit[] m_AllUnit = null;//所有的按紐
+    [SerializeField] Text m_HpText = null;//生命值文字
+    [SerializeField] Text m_ScoreText = null;//分數文字
+    [SerializeField] ManagerUnit[] m_AllUnit = null;//所有的按紐
 
-    private int _m_Hp = 5;//生命值預設為5
-    private int m_Hp
-    {//生命值更新
-        get
-        {
-            return _m_Hp;
-        }
-        set
-        {
-            _m_Hp = value;
-            m_HpText.text = _m_Hp.ToString();//顯示更新生命值
-        }
-    }
+    //private int _m_Hp = 5;//生命值預設為5
+    //private int m_Hp
+    //{//生命值更新
+    //    get
+    //    {
+    //        return _m_Hp;
+    //    }
+    //    set
+    //    {
+    //        _m_Hp = value;
+    //        m_HpText.text = _m_Hp.ToString();//顯示更新生命值
+    //    }
+    //}
 
-    private int _m_Score = 0;//分數預設為0
-    private int m_Score
-    {//分數更新
-        get
-        {
-            return _m_Score;
-        }
-        set
-        {
-            _m_Score = value;
-            m_ScoreText.text = _m_Score.ToString();//顯示更新分數
-        }
-    }
+    //private int _m_Score = 0;//分數預設為0
+    //private int m_Score
+    //{//分數更新
+    //    get
+    //    {
+    //        return _m_Score;
+    //    }
+    //    set
+    //    {
+    //        _m_Score = value;
+    //        m_ScoreText.text = _m_Score.ToString();//顯示更新分數
+    //    }
+    //}
 
-    private bool m_PlayerLive = true;//玩家存活
+    //private bool m_PlayerLive = true;//玩家存活
     //private int m_HankRank = 1;//玩家等級1
 
-    private bool isCountingDown = false; // 是否正在倒數
-    private float countdownTime = 5f; // 倒數的初始時間
-    private float countdownTimer = 0f; // 倒數計時器
-    private float lastClickTime = 0f; // 上次點擊的時間
+    //private bool isCountingDown = false; // 是否正在倒數
+    //private float countdownTime = 5f; // 倒數的初始時間
+    //private float countdownTimer = 0f; // 倒數計時器
+    //private float lastClickTime = 0f; // 上次點擊的時間
     //private float doubleClickThreshold = 0.3f; //雙擊值域
     //private bool isDoubleClick = false; // 是否是雙擊
 
-    public void Awake()
+    /*public void Awake()
     {
         m_instance = this;
-    }
-    void Start()
-    {
-        // 倒計時
-        StartCountdown();
-    }
+    }*/
+    //void Start()
+    //{
+    //    // 倒計時
+    //    StartCountdown();
+    //}
 
     void Update()
     {
         for (int i = 0; i < m_AllUnit.Length; i++)
         {
             ManagerUnit unit = m_AllUnit[i];
-            if (unit.CheckTime() == true)
+            if (unit.CheckTime())
             {//檢查按鈕重新生成時間，若到達則重新生成
-                unit.Reburn();
+                unit.SwitchActive();
             }
             else
             {
@@ -88,133 +88,133 @@ public class GameManager : MonoBehaviour
 
         }
 
-        if (!m_PlayerLive)
-            return;
+    //    if (!m_PlayerLive)
+    //        return;
 
-        if (!isCountingDown)
-        {
-            // 如果沒有在倒數，檢查是否需要開始倒數
-            bool hasClick = Input.GetMouseButtonDown(0);
+    //    if (!isCountingDown)
+    //    {
+    //        // 如果沒有在倒數，檢查是否需要開始倒數
+    //        bool hasClick = Input.GetMouseButtonDown(0);
 
-            //檢查玩家有無開始點擊
-            if (!hasClick)
-            {
-                // 如果沒有點擊，開始倒數計時
-                countdownTimer -= Time.deltaTime;
+    //        //檢查玩家有無開始點擊
+    //        if (!hasClick)
+    //        {
+    //            // 如果沒有點擊，開始倒數計時
+    //            countdownTimer -= Time.deltaTime;
 
-                // 開始倒數時，生命值減少
-                if (countdownTimer <= 0f)
-                {
-                    this.m_Hp--;
+    //            // 開始倒數時，生命值減少
+    //            if (countdownTimer <= 0f)
+    //            {
+    //                this.m_Hp--;
 
-                    //生命值減少至0則玩家死亡
-                    if (this.m_Hp == 0)
-                    {
-                        PlayerDie();
-                    }
+    //                //生命值減少至0則玩家死亡
+    //                if (this.m_Hp == 0)
+    //                {
+    //                    PlayerDie();
+    //                }
                         
-                    // 若生命值在為未減少為0時有點擊，則重置倒數計時器
-                    else
-                    {
+    //                // 若生命值在為未減少為0時有點擊，則重置倒數計時器
+    //                else
+    //                {
                         
-                        countdownTimer = countdownTime;
-                    }
-                }
-            }
-            else
-            {
-                // 如果有點擊
-                AddScore(1); // 單擊增加一分
-                countdownTimer = countdownTime;//重置倒數計時器
-            }
+    //                    countdownTimer = countdownTime;
+    //                }
+    //            }
+    //        }
+    //        else
+    //        {
+    //            // 如果有點擊
+    //            AddScore(1); // 單擊增加一分
+    //            countdownTimer = countdownTime;//重置倒數計時器
+    //        }
 
-            // 設定為正在倒數狀態
-            isCountingDown = true;
-        }
-        else
-        {
-            // 如果正在倒數，檢查是否有點擊，若有則重置倒數計時器
-            if (Input.GetMouseButtonDown(0))
-            {
-                countdownTimer = countdownTime;
-            }
-        }
-    }
-    private void StartCountdown()
-    {//開始倒數計時的協程
-        StartCoroutine(Countdown());
-    }
+    //        // 設定為正在倒數狀態
+    //        isCountingDown = true;
+    //    }
+    //    else
+    //    {
+    //        // 如果正在倒數，檢查是否有點擊，若有則重置倒數計時器
+    //        if (Input.GetMouseButtonDown(0))
+    //        {
+    //            countdownTimer = countdownTime;
+    //        }
+    //    }
+    //}
+    //private void StartCountdown()
+    //{//開始倒數計時的協程
+    //    StartCoroutine(Countdown());
+    //}
 
-    private IEnumerator Countdown()
-    {//協程
-        for (; this.m_Hp > 0; this.m_Hp--)
-            yield return new WaitForSeconds(1f); // 等待1秒
+    //private IEnumerator Countdown()
+    //{//協程
+    //    for (; this.m_Hp > 0; this.m_Hp--)
+    //        yield return new WaitForSeconds(1f); // 等待1秒
 
-        PlayerDie();
-    }
+    //    PlayerDie();
+    //}
 
-    public void OnClick()
-    {
-        this.m_Hp = 5;
-        //float currentTime = Time.time;
-        // 當有點擊時，重置倒數計時器
-        countdownTimer = countdownTime;//重製倒數時間
-        isCountingDown = false;//點擊後停止倒數
+    //public void OnClick()
+    //{
+    //    this.m_Hp = 5;
+    //    //float currentTime = Time.time;
+    //    // 當有點擊時，重置倒數計時器
+    //    countdownTimer = countdownTime;//重製倒數時間
+    //    isCountingDown = false;//點擊後停止倒數
 
-        /*if (currentTime - lastClickTime <= doubleClickThreshold)
-        {
-            // 雙擊
-            // AddScore(2);
-            isDoubleClick = true;
-        }
-        else
-        {
-            // 單擊
-            AddScore(1);
-            isDoubleClick = false;
-        }*/
+    //    /*if (currentTime - lastClickTime <= doubleClickThreshold)
+    //    {
+    //        // 雙擊
+    //        // AddScore(2);
+    //        isDoubleClick = true;
+    //    }
+    //    else
+    //    {
+    //        // 單擊
+    //        AddScore(1);
+    //        isDoubleClick = false;
+    //    }*/
 
-        //lastClickTime = currentTime;
-    }
+    //    //lastClickTime = currentTime;
+    //}
 
-    /*public int GetHardRank()
-    {
-        return m_HankRank;
-    }*/
+    ///*public int GetHardRank()
+    //{
+    //    return m_HankRank;
+    //}*/
 
-    public void Hit()
-    {//處理按鈕點擊時的狀況
+    //public void Hit()
+    //{//處理按鈕點擊時的狀況
 
-        this.m_Hp -= 1;//生命值-1
+    //    this.m_Hp -= 1;//生命值-1
 
-        if (this.m_Hp == 0)
-            PlayerDie();//玩家死亡
-    }
+    //    if (this.m_Hp == 0)
+    //        PlayerDie();//玩家死亡
+    //}
 
-    private void PlayerDie()
-    {
-        m_PlayerLive = false;//玩家死亡
-    }
+    //private void PlayerDie()
+    //{
+    //    m_PlayerLive = false;//玩家死亡
+    //}
 
-    public void AddScore(int pointsToAdd)
-    {
-        if (!m_PlayerLive)//玩家是否活著
-            return;
+    //public void AddScore(int pointsToAdd)
+    //{
+    //    if (!m_PlayerLive)//玩家是否活著
+    //        return;
 
-        /*if (m_HankRank == 1 && m_Score > 5)
-        {
-            m_HankRank = 2;
-        }
-        else if(m_HankRank == 2 && m_Score > 30)
-        {
-            m_HankRank = 3;
-        }
-        // 如果是雙擊，多加兩分
-        if (isDoubleClick)
-            n += 2;*/
-        m_Score += pointsToAdd; // 增加分數
-        m_ScoreText.text = m_Score.ToString(); // 更新分數
-        //this.m_Score += n;
+    //    /*if (m_HankRank == 1 && m_Score > 5)
+    //    {
+    //        m_HankRank = 2;
+    //    }
+    //    else if(m_HankRank == 2 && m_Score > 30)
+    //    {
+    //        m_HankRank = 3;
+    //    }
+    //    // 如果是雙擊，多加兩分
+    //    if (isDoubleClick)
+    //        n += 2;*/
+    //    m_Score += pointsToAdd; // 增加分數
+    //    m_ScoreText.text = m_Score.ToString(); // 更新分數
+    //    //this.m_Score += n;
     }
 
 }
--- Assets/Scipt/ManagerUnit.cs
+++ Assets/Scipt/ManagerUnit.cs
@@ -6,158 +6,121 @@ using UnityEngine.UI;
 
 public class ManagerUnit : MonoBehaviour
 {
-   
-    private float m_StartTime = 0;//按鈕出現時間
-    private float m_CloseTime = 0;//消失按鈕時間
-    private bool m_IsLive = false;//按鈕是否存活
-    private bool m_IsCanClick = false;//是否點擊按鈕
+    
+    private float m_Time;
+    //private bool m_IsLive = false;//按鈕是否存活
+    //private bool m_IsCanClick = false;//是否點擊按鈕
 
-    private float m_Hp=0;//生命值
-    private float m_RemoveTime = 0;//移除時間
-    private int m_MaxHp;//最大生命值
-    private Dictionary<ManagerUnit, int> m_MaxHpDict = new Dictionary<ManagerUnit, int>();
+    //private float m_Hp=0;//生命值
+    //private int m_MaxHp;//最大生命值
+    //private Dictionary<ManagerUnit, int> m_MaxHpDict = new Dictionary<ManagerUnit, int>();
 
-    private Text m_HitTimes = null;
+    //private Text m_HitTimes = null;
 
-    private void Awake()
-    {//點擊次數顯示
-        m_HitTimes = gameObject.GetComponentInChildren<Text>();
+    
+    void Start()
+    {
+        Disable();
+        //m_Hp = m_MaxHp; // 設定按鈕的當前生命值為最大生命值
     }
+    //private void Awake()
+    //{//點擊次數顯示
+    //    m_HitTimes = gameObject.GetComponentInChildren<Text>();
+    //}
 
     public void OnClickMonster()
     {//按鈕點擊事件
         gameObject.SetActive(false);
-        if (m_IsCanClick == true)
-        {
-            return;
-        }
-        m_Hp--;//如果不點擊則生命值-1
+        //if (m_IsCanClick == true)
+        //{
+        //    return;
+        //}
+        //m_Hp--;//如果不點擊則生命值-1
 
-        m_HitTimes.text = m_Hp.ToString();//更新點擊次數
-        if (m_Hp <= 0)
-        {//檢查生命值是否<0
-            //int score = (GameManager.Instance.GetHardRank() == 1) ? 1 : 2;
-            GameManager.Instance.AddScore(1);
-            OnDie();//按鈕消失
-        }
-        m_IsLive = false;//無按鈕
+        //m_HitTimes.text = m_Hp.ToString();//更新點擊次數
+        //if (m_Hp <= 0)
+        //{//檢查生命值是否<0
+        //    //int score = (GameManager.Instance.GetHardRank() == 1) ? 1 : 2;
+        //    GameManager.Instance.AddScore(1);
+        //    OnDie();//按鈕消失
+        //}
+        //m_IsLive = false;//無按鈕
     }
-    void Start()
+    //void Update()
+    //{
+
+
+    //    if (m_IsLive)
+    //    {
+    //        if (transform.localScale.x < 1)
+    //        {//按紐大小生成中
+    //            transform.localScale += new Vector3(1f, 1f, 1f) * Time.deltaTime;
+    //        }
+    //        else
+    //        {//按鈕生成至一定大小才可點擊
+    //            m_IsCanClick = true;
+
+    //        }
+
+    //        if (m_IsCanClick)
+    //        {
+    //            //m_RemoveTime += Time.deltaTime;//按鈕已存在時間
+    //            /*int limitTime = 5 - GameManager.Instance.GetHardRank();
+    //            if (m_RemoveTime > limitTime)
+    //            {//按鈕未在已存在時間內點擊
+    //                GameManager.Instance.Hit();
+    //                OnDie();//按鈕消失
+    //            }*/
+    //        }
+    //    }
+    //}
+
+
+
+    //private void OnDie()
+    //{//按鈕消失
+    //    StartCoroutine(FadeOut());//漸隱效果
+    //    gameObject.SetActive(false);//無按鈕
+    //    m_IsLive = false;//按鈕不出現
+    //    //AddStartTime();//按鈕出現時間
+    //}
+
+    //private IEnumerator FadeOut()
+    //{
+    //    float duration = 1.0f;//按鈕出現到消失的時間
+    //    float elapsedTime = 0.01f;//按鈕剛出現時間
+
+    //    while (elapsedTime < duration)
+    //    {
+    //        float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration); // 按鈕漸隱效果
+    //        Color newColor = new Color(1f, 1f, 1f, alpha); // 得到按鈕完整顏色效果
+    //        gameObject.GetComponent<Renderer>().material.color = newColor;
+
+    //        elapsedTime += Time.deltaTime; // 更新已過去的時間
+    //        yield return null; // 等待下一幀
+    //    }
+
+    //    gameObject.SetActive(true);
+    //    m_IsLive = true; // 有按鈕
+    //    //AddStartTime(); 
+    //}
+
+    public bool CheckTime() => Time.time > m_Time;
+ 
+    public void Disable()
     {
-        AddStartTime();
-        m_Hp = m_MaxHp; // 設定按鈕的當前生命值為最大生命值
+        gameObject.SetActive(false);
+        AddTime();
     }
 
-    void Update()
+    private void AddTime()
     {
-        if (Time.time > m_StartTime)
-        {//出現時間後才顯示按鈕
-            AddStartTime();
-            gameObject.SetActive(false);//有按紐
-        }
-
-        if (Time.time > m_CloseTime)
-        {//消失時間後才顯示按鈕
-            AddCloseTime();
-            gameObject.SetActive(true);//無按鈕
-        }
-
-        if (m_IsLive)
-        {
-            if (transform.localScale.x < 1)
-            {//按紐大小生成中
-                transform.localScale += new Vector3(1f, 1f, 1f) * Time.deltaTime;
-            }
-            else
-            {//按鈕生成至一定大小才可點擊
-                m_IsCanClick = true;
-
-            }
-
-            if (m_IsCanClick)
-            {
-                m_RemoveTime += Time.deltaTime;//按鈕已存在時間
-                /*int limitTime = 5 - GameManager.Instance.GetHardRank();
-                if (m_RemoveTime > limitTime)
-                {//按鈕未在已存在時間內點擊
-                    GameManager.Instance.Hit();
-                    OnDie();//按鈕消失
-                }*/
-            }
-        }
+        m_Time = Time.time + Random.Range(1f, 3f);
     }
 
-
-
-    private void OnDie()
-    {//按鈕消失
-        StartCoroutine(FadeOut());//漸隱效果
-        gameObject.SetActive(false);//無按鈕
-        m_IsLive = false;//按鈕不出現
-        AddStartTime();//按鈕出現時間
-    }
-
-    private IEnumerator FadeOut()
+    public void SwitchActive()
     {
-        float duration = 1.0f;//按鈕出現到消失的時間
-        float elapsedTime = 0.01f;//按鈕剛出現時間
-
-        while (elapsedTime < duration)
-        {
-            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / duration); // 按鈕漸隱效果
-            Color newColor = new Color(1f, 1f, 1f, alpha); // 得到按鈕完整顏色效果
-            gameObject.GetComponent<Renderer>().material.color = newColor;
-
-            elapsedTime += Time.deltaTime; // 更新已過去的時間
-            yield return null; // 等待下一幀
-        }
-
-        gameObject.SetActive(true); 
-        m_IsLive = true; // 有按鈕
-        AddStartTime(); 
-    }
-
-    public bool CheckTime() => Time.time > m_StartTime;//檢查重新出現時間是否已大於當前時間
-
-    public void Reburn()
-    {//如果無按鈕則重新生成按鈕
-
-        Debug.Log("Reburn() called from: " + transform.name);
-        if (m_IsLive == false)//無按鈕生成
-        {   
-
-            // 創建一個新的按鈕
-            GameObject newUnitObj = Instantiate(this.gameObject);
-            ManagerUnit newUnit = newUnitObj.GetComponent<ManagerUnit>();
-
-            // 設定新按鈕的生成時間和消失時間
-            newUnit.AddStartTime();
-            newUnit.AddCloseTime();
-
-            
-            newUnit.m_IsLive = false;//新按鈕存活
-            newUnit.m_IsCanClick = true;//按鈕生成後可點擊
-            newUnit.transform.localScale = new Vector3(1f, 1f, 1f);//按鈕的初始縮放
-            newUnit.m_Hp = m_MaxHp;// 設定新按鈕的最大生命值
-            newUnit.m_RemoveTime = 0;//移除重製時間
-            //AddStartTime();//生重新的出現時間
-            //newUnit.m_HitTimes.text = m_Hp.ToString();//更新點擊次數
-            
-            
-            //m_Hp = GameManager.Instance.GetHardRank();//設定生命值為當前難度
-            newUnitObj.SetActive(true);//顯示新按鈕
-          } 
-            
-        
-    }
-
-    private void AddStartTime()
-    {//按鈕隨機出現
-        m_StartTime = Time.time + Random.Range(1f, 3f);
-    }
-
-    private void AddCloseTime()
-    {//按鈕隨機消失
-        m_CloseTime = Time.time + Random.Range(1f, 3f);
+        gameObject.SetActive(!gameObject.activeSelf);
+        AddTime();
     }
 }
